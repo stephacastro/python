10080b60e4e589ccfd870d6f4fc383103d11bed2
Stephanie de Castro Silva <stephanie.silva@yaman.com.br>
2023-01-16 21:57:58 -0300

    commit

--- cursopython/basico/modulo2/game.py
+++ cursopython/basico/modulo2/game.py
@@ -1,34 +1,68 @@
 """ Jogo da forca """
 
+import random
+
 def jogar():
+    imprime_boasvindas()
+    palavra_secreta = carrega_palavra()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+   
+   
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+
+    while(not enforcou and not acertou):
+
+        chute = input('Qual letra? ').strip().upper()
+        
+
+        if(chute in palavra_secreta):
+            index = 0
+
+            for letra in palavra_secreta:                                         
+                if(chute == letra):
+                    letras_acertadas[index] = letra    
+                index = index + 1
+
+        else:
+            erros = erros + 1    
+
+        enforcou = erros == 6 
+        acertou = "_" not in letras_acertadas      
+        print(letras_acertadas)
+
+    if (acertou):
+        print('Parabéns, Você ganhou!!!')
+    else:
+        print(f'Você perdeu! a palavra é {palavra_secreta}')
+    
+    print('Fim do Jogo')
+
+def imprime_boasvindas():
     print('*'*40)
     print('*** Bem vindo ao Jogo da Forca! ***')
     print('*'*40)
 
-    palavra_secreta = 'banana'
+def carrega_palavra():
+    caminho = r'C:\python\cursopython\basico\modulo2'
+    arquivo = open(f"{caminho}\palavras.txt", "r")
+    palavras = []
 
-    enforcou = False
-    acertou = False
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
 
-    while(not enforcou and not acertou):
+    arquivo.close
 
-        chute = input('Qual letra? ').strip()
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta 
 
-        index = 0
-
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print(f"Encontrei a letra {chute} na posição {index}")
-            
-            index = index + 1
-
-
-        print('jogando...')
-
-
-
-
-    print('Fim do Jogo')
-
-if (__name__ == "__main__"):
-    jogar()
+def inicializa_letras_acertadas(palavra_secreta):   
+    return ["_" for letra in palavra_secreta]
+    
+jogar()

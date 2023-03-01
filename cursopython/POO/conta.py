d694694146aed8e903bb1469dd887ccec3ef426f
Stephanie de Castro Silva <stephanie.silva@yaman.com.br>
2023-02-28 21:19:05 -0300

    'commit'

--- cursopython/POO/conta.py
+++ cursopython/POO/conta.py
@@ -5,23 +5,26 @@ class Conta:
     # self - acessa as propriedades de uma instância
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo um objeto... {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     
     def extrato(self):
-        print(f'Saldo de {self.saldo} do titular {self.titular}')
+        print(f'Saldo de {self.__saldo} do titular {self.__titular}')
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
 
 conta = Conta(152, 'Stephanie', 173.5, 1000.0)
 conta2 = Conta(176, 'Manu', 90.0, 2000.0)
 
-# chamando o objeto e acessando a função
-conta.extrato()
+# chamando o objeto e acessando a função encapsulado
+conta._Conta__saldo
+
+# chamando o objeto e acessando a função 
+conta2.extrato()

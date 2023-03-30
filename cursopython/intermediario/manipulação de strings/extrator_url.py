38a2ff5d8b3bfe3c7987954530b66e7049c862fb
Stephanie de Castro Silva <stephanie.silva@yaman.com.br>
2023-03-29 21:51:10 -0300

    'commit'

--- cursopython/intermediario/manipulação de strings/extrator_url.py
+++ cursopython/intermediario/manipulação de strings/extrator_url.py
@@ -39,8 +39,17 @@ class ExtratorURL:
         else:
             valor = self.get_url_parametro()[indice_valor:indice_comercial]
         return valor
+    
+    def __len__(self):
+        return len(self.url)
+    
+    def __str__(self):
+        return self.url
 
 
-extrator_url = ExtratorURL('https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar')
+url = 'https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar'
+extrator_url = ExtratorURL(url)
+print('O tamanho da URL: ', len(extrator_url))
+
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)

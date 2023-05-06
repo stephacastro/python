af4eb35f0f1ec99d1e9b53bba8d1c3e3103eff88
Stephanie de Castro Silva <stephanie.silva@yaman.com.br>
2023-05-05 22:06:22 -0300

    commit

--- cursopython/testes_auto/bank.py
+++ cursopython/testes_auto/bank.py
@@ -20,11 +20,28 @@ class Funcionario:
         ano_nascimento = data_nascimento_quebrada[-1]
         ano_atual = date.today().year
         return ano_atual - int(ano_nascimento)
+    
+    def sobrenome(self):
+        nome_completo = self.nome.strip()
+        nome_quebrado = nome_completo.split(' ')
+        return nome_quebrado[-1]
 
+    def _socio(self):
+        sobrenomes = ['Bragança', 'Windsor', 'Bourbon', 'Yamato', 'Al Saud', 'Khan', 'Tudor', 'Plolomeu']
+        if self._salario >= 100000 and (self.sobrenome() in sobrenomes):
+            return True
+        else:
+            return False
+
+    def decrescimo_salario(self):
+        if self._socio():
+            decrescimo = self._salario * 0.1
+            self._salario = self._salario - decrescimo
+        
     def calcular_bonus(self):
         valor = self._salario * 0.1
         if valor > 1000:
-            valor = 0
+            raise Exception('O salário é muito alto para receber um bônus')
         return valor 
 
     def __str__(self):
--- cursopython/testes_auto/tests/test_bank.py
+++ cursopython/testes_auto/tests/test_bank.py
@@ -1,6 +1,9 @@
 from bank import Funcionario
+import pytest
+from pytest import mark
 
 class TestClass:
+
     def test_quando_idade_recebe_21_09_1997_deve_retornar_26(self):
         entrada = '21/09/1997'
         esperado = 26
@@ -9,3 +12,51 @@ class TestClass:
         resultado = funcionario_teste.idade()
 
         assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_Stephanie_Castro_deve_retornar_Castro(self):
+        entrada =  ' Stephanie Castro ' 
+        esperado = 'Castro'
+
+        ste = Funcionario(entrada, '21/09/1997', 5000)
+
+        resultado = ste.sobrenome()
+
+        assert resultado == esperado
+
+    #@mark.skip  # pula um teste especifico
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000
+        entrada_nome = 'Maria Yamato'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+
+        funcionario_teste.decrescimo_salario()
+
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
+
+    @mark.calcular_bonus # tag associado a um determinado teste
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000 #given
+        esperado = 100
+
+        calcular = Funcionario('Teste', '11/12/1997', entrada)
+
+        resultado = calcular.calcular_bonus() # when
+
+        assert resultado == esperado # then
+
+    @mark.calcular_bonus
+    # teste com exceção de erro
+    def test_quando_calcular_bonus_recebe_10000000_deve_retornar_exeption(self):
+        with pytest.raises(Exception):
+            entrada = 10000000 #given
+
+            calcular = Funcionario('Teste', '11/12/1997', entrada)
+
+            resultado = calcular.calcular_bonus() # when
+
+            assert resultado # then
+        
